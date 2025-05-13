be110688432f719155084c359cef168496c20ba8
DarshnikRohal <129205882+DarshnikRohal@users.noreply.github.com>
2025-05-13 12:01:11 +0530

    Update App.jsx

--- Day-04/code/App.jsx
+++ Day-04/code/App.jsx
@@ -1,1 +1,17 @@
+import { useState } from "react";
+
+export default function App() {
+  const [count,setCount]=useState(0);
+  function onClickhandler(){
+    setCount(count+1);
+  }
+  return (
+    <div>
+      <button onClick={onClickhandler}>Counter {count}
+      </button>
+    </div>
+  );
+}
+
+
 
