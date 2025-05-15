63aa2a75ea22135107a3f694c8d1f8e8a5bad7ae
DarshnikRohal <129205882+DarshnikRohal@users.noreply.github.com>
2025-05-15 14:05:09 +0530

    Update App.jsx

--- Day-05/Code/App.jsx
+++ Day-05/Code/App.jsx
@@ -1,1 +1,29 @@
+import { useState, useEffect } from "react";
+import Counter from "./Counter";
+import Message from "./Message";
+
+const App = () => {
+  const [showCounter, setShowCounter] = useState(true);
+
+  useEffect(() => {
+    console.log("App mounted!");
+
+    return () => {
+      console.log("App unmounted or re-rendered (cleanup)");
+    };
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+      <h1>🧠 React Hooks Demo</h1>
+      <button onClick={() => setShowCounter(!showCounter)}>
+        {showCounter ? "Hide" : "Show"} Counter
+      </button>
+      {showCounter && <Counter initial={0} />}
+      <Message text="Keep Reacting!" />
+    </div>
+  );
+};
+
+export default App;
 
